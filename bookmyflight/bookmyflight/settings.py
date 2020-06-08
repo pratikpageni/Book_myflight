7cf7879a38148fc1632f1792389c1097ea519957
Sajan4072 <rai4072@gmail.com>
2020-06-08 17:56:58 +0545

    postgresql and  migration

--- bookmyflight/bookmyflight/settings.py
+++ bookmyflight/bookmyflight/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flights'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +78,16 @@ WSGI_APPLICATION = 'bookmyflight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER':'postgres',
+        'PASSWORD':'x',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
+        
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- bookmyflight/flights/views.py
+++ bookmyflight/flights/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
 # Create your views here.
+def index(request):
+    print(os.environ['PASSWORD'])
+
+    return HttpResponse ("hello")
