352e870c186386cc6962c043cf5b8525725b05df
Sajan4072 <rai4072@gmail.com>
2020-06-10 18:58:14 +0545

    flight view

--- bookmyflight/flights/views.py
+++ bookmyflight/flights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import os
 from .models import Flight
 
@@ -11,3 +11,13 @@ def index(request):
    
 
     return render (request,"flights/index.html",context)
+
+def flight(self):
+    try:
+        flight=Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight does not exist.")
+    context={
+        "flight":flight
+    }
+    return render (request,"flights/flight.html",context)
