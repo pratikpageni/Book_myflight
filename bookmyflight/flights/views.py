d4bf1f7f82b5b8d31f2fac6abf957a9608317560
Sajan4072 <rai4072@gmail.com>
2020-06-10 17:26:35 +0545

    template index

--- bookmyflight/flights/views.py
+++ bookmyflight/flights/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+from .models import Flight
 
 # Create your views here.
 def index(request):
+    context={
+      "flights":Flight.objects.all()  
+    }
    
 
-    return HttpResponse ("flights")
+    return render (request,"flights/index.html",context)
