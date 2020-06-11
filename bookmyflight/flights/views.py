25530cc66793bd8c6fa59888e5af5a412a98c4b3
Sajan4072 <rai4072@gmail.com>
2020-06-11 17:11:58 +0545

    book view

--- bookmyflight/flights/views.py
+++ bookmyflight/flights/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 import os
-from .models import Flight
+from .models import Flight,Airport,Passenger
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -22,5 +23,26 @@ def flight(request,flight_id):
         "passengers":flight.passengers.all()
     }
     return render (request,"flights/flight.html",context)
+
+
+def book(request,flight_id):
+    try:
+        passenger_id=int(request.POST["passenger"])
+        passenger=Passenger.objects.get(pk=passenger_id)
+        flight=Flight.objects.get(pk=flight_id)
+    except KeyError:
+        return render(request,"flights/error.html",{"message":"No selection"})
+
+    
+    except Flight.DoesNotExist:
+        return render(request,"flights/error.html",{"message":"No flight"})
+    
+    except Passenger.DoesNotExist:
+        return render(request,"flights/error.html",{"message":"No Passenger"})
+
+    passenger.flights.add(flight) 
+    return HttpResponseRedirect(reverse("flights",args=(flight_id,))) 
+
+
 
 
